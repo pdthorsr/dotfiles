c7573bf868caca8af07d30db3c8e7ed5089c784c
Thorsrud <m88614@u1804i-0481.SKEAD.NO>
2020-03-04 10:30:54 +0100

    Deleted all the unnecessary stuff

--- config/chromium/Default/Extensions/hdokiejnpimakedhajhdlcegeplioahd/4.30.0.2_0/infieldPopupInterface.js
+++
@@ -1,2 +0,0 @@
-Interfaces.InfieldPopupInterface=function(){var e,n;return{lpInfield:{setArrowPosition:new(0,Interfaces.Definition)(Interfaces.TYPE_FUNCTION,{include:["contentScript"]})}}}();
-//# sourceMappingURL=sourcemaps/infieldPopupInterface.js.map
