04ae4dad2bae962f81cb1b50ea104ff4018b5946
Per Daniel Thorsrud <perdaniel.thorsrud@skatteetaten>
2020-05-14 14:00:28 +0200

    not working code. Added code for creating symlinks

--- scripts/create_symlinks.py
+++ scripts/create_symlinks.py
@@ -1,5 +1,13 @@
 import os
-from py_dotenv import read_dotenv
+from dotenv import load_dotenv
 
-read_dotenv("../dots/envfile")
-print(os.getenv("MINERVADIR"))
+load_dotenv("../dots/envfile")
+dotfilesdir = os.getenv("DOTFILESDIR")
+homedir = "/home/" + os.getenv("SCRIPTUSERNAME")
+homedirfiles = os.listdir(homedir)
+for file in os.listdir(dotfilesdir):
+    new_file_name = "." + file
+    # print(new_file_name)
+    if new_file_name not in homedirfiles:
+        print(new_file_name)
+        os.link("{}/{}".format(dotfilesdir, file), "{}{}".format(homedir, new_file_name))
